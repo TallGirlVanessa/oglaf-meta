0d2c571f3a9637b30a42702cd803901dfe8ef63b
Vanessa Phipps <vlmphipps@gmail.com>
2023-01-30 12:43:30 -0500

    Search: validate length min 3 max 50

--- oglaf/features/tag_search/tag_search_forms.py
+++ oglaf/features/tag_search/tag_search_forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class TagSearchForm(FlaskForm):
-    search = StringField("Search", validators=[DataRequired()])
+    search = StringField("Search", validators=[DataRequired(), Length(min=3, max=50)])
